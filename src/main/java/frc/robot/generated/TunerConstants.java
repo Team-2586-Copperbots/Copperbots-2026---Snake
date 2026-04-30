8d630ec752be581fc2341f8d28e103a8a5fc0535
northforest2 <northforest2@gmail.com>
2026-04-30 12:22:46 -0400

    use stear config

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -73,7 +73,7 @@ public class TunerConstants {
                                                         // stator current limit to help avoid brownouts without
                                                         // impacting performance.
                                                         .withStatorCurrentLimit(Amps.of(20))
-                                                        .withStatorCurrentLimitEnable(true));
+                                                        .withSupplyCurrentLimit(Amps.of(20)));
         private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
         // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
         private static final Pigeon2Configuration pigeonConfigs = null;
